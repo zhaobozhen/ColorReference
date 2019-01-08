37c22de825adad45e072a95175089cc32f4a1d2c
zhaobozhen <zhaobozhen2025@gmail.com>
2019-01-08 15:30:22 +0800

    Minor bugs

--- app/src/main/java/com/by_syk/mdcolor/fragment/HelpDialog.java
+++ app/src/main/java/com/by_syk/mdcolor/fragment/HelpDialog.java
@@ -1,17 +1,17 @@
-/**
- * Copyright 2016-2017 By_syk
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+/*
+  Copyright 2016-2017 By_syk
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
  */
 
 package com.by_syk.mdcolor.fragment;
@@ -20,9 +20,6 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
-import android.text.Spannable;
-import android.text.SpannableString;
-import android.text.style.UnderlineSpan;
 
 import com.by_syk.lib.storage.SP;
 import com.by_syk.mdcolor.BaseActivity;
